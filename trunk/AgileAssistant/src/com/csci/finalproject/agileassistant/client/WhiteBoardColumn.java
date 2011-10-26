76ec476e13445b10786abc49b44ccc4490a017ae
burtonjc35 <burtonjc35@8a08e298-b036-26f9-6304-ede39f763e67>
2011-10-26 23:25:04 +0000

    This is just commiting the WhiteBoardColumn class stub. this class still needs to be implemented.

---
+++ trunk/AgileAssistant/src/com/csci/finalproject/agileassistant/client/WhiteBoardColumn.java
@@ -0,0 +1,10 @@
+package com.csci.finalproject.agileassistant.client;
+
+import com.google.gwt.user.client.ui.Composite;
+
+public class WhiteBoardColumn extends Composite {
+
+	public WhiteBoardColumn() {
+	}
+
+}
