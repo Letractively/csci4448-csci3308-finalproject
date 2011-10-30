b5e98ca02811a108b376b8883331216e281c8a5a
hebadi89@gmail.com <hebadi89@gmail.com@8a08e298-b036-26f9-6304-ede39f763e67>
2011-10-30 20:16:35 +0000

    Task.java - added getters and setter.  Just had to update to head again and it allowed me to add the getters/setters.
    also not sure about the "import java.jdo......" it was the first option to get rid of the errors on IdGen, PersistenceCapable, Persistent, PKey

--- trunk/AgileAssistant/src/com/csci/finalproject/agileassistant/server/Task.java
+++ trunk/AgileAssistant/src/com/csci/finalproject/agileassistant/server/Task.java
@@ -1,5 +1,12 @@
 package com.csci.finalproject.agileassistant.server;
 
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.users.User;
+
 @PersistenceCapable(identityType = IdentityType.APPLICATION)
 public class Task {
 	
@@ -10,11 +17,39 @@ public class Task {
 	@Persistent
 	private String title;
 	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getCondition() {
+		return condition;
+	}
+
+	public void setCondition(int condition) {
+		this.condition = condition;
+	}
+
+	public long getID() {
+		return ID;
+	}
+
+	public int getTask_numb() {
+		return task_numb;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
 	@Persistent
 	private int task_numb; //is this task 1, 2, 3,....
 	
 	@Persistent
-	private int condition; // 0=To Do 1=In Prof 2=In Veriification 3= Complete
+	private int condition; // 0=To Do 1=In Progress 2=In Verification 3= Complete
 
 	@Persistent
 	private User user;
