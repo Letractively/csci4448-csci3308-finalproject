ec597b35f2e053af86c4480b3e3f105ad165a7b9
hebadi89@gmail.com <hebadi89@gmail.com@8a08e298-b036-26f9-6304-ede39f763e67>
2011-11-06 03:22:33 +0000

    testfiles for server side (Junit)

---
+++ trunk/AgileAssistant/test/com/csci/finalproject/agileassistant/server/jiggaTest.java
@@ -0,0 +1,14 @@
+package com.csci.finalproject.agileassistant.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class jiggaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ trunk/AgileAssistant/test/com/csci/finalproject/agileassistant/server/testTask.java
@@ -0,0 +1,22 @@
+package com.csci.finalproject.agileassistant.server;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.junit.client.GWTTestCase;
+
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.junit.client.GWTTestCase;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public class testTask extends GWTTestCase {
+
+	public String getModuleName() {
+		return "com.csci.finalproject.agileassistant.AgileAssistant";
+	}
+	
+	public void testConstruction(){
+		assert(true);
+	}
+
+}
