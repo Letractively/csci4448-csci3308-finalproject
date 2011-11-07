b9b207722d6f45078cf2be5fbeb740e847aaf2e6
hebadi89 <hebadi89@gmail.com>
2011-11-07 01:24:04 +0000

    task.java unit tests  -  several are failing, most concerning is the KEY value is not being generated

--- AgileAssistant/test/com/csci/finalproject/agileassistant/server/testTask.java
+++ AgileAssistant/test/com/csci/finalproject/agileassistant/server/testTask.java
@@ -1,22 +1,80 @@
 package com.csci.finalproject.agileassistant.server;
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.junit.client.GWTTestCase;
+import static org.junit.Assert.*;
 
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.junit.client.GWTTestCase;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.rpc.AsyncCallback;
+import org.junit.Before;
+import org.junit.Test;
 
-public class testTask extends GWTTestCase {
+import com.csci.finalproject.agileassistant.client.TaskData;
+import com.google.appengine.api.datastore.Key;
 
-	public String getModuleName() {
-		return "com.csci.finalproject.agileassistant.AgileAssistant";
+public class testTask {
+
+	
+	@Test
+	public void test(){
+		String title = "Test Title";
+		UserStory userStory = null;//new UserStory();
+		int task_numb = 1;
+		Task testTask = new Task(title, userStory, task_numb);
+		
+		//title test
+		String title_check = "this is a new title";
+		testTask.setTitle(title_check);
+		if(title_check == "this is a new title")
+			assert(true);
+		else
+			fail("get or set title error has occured");
+		
+		//test key get
+		Key tmp_key = testTask.getKey();
+		if(tmp_key != null)
+			assert(true);
+		else
+			fail("key is equal to NULL, test failed");
+		
+		//test Task numb
+		testTask.setTask_numb(4);
+		int tmp_tasknum = testTask.getTask_numb();
+		//assertEquals(tmp_tasknum, 4);
+		if(tmp_tasknum == 4)
+			assert(true);
+		else
+			fail("task number unable to be set");
+		
+		//test Condition setting
+		int condition = 0;
+		int tmp_condition;
+		tmp_condition = testTask.getCondition();
+		if(tmp_condition == condition)
+			testTask.setCondition(condition+1);
+		else
+			testTask.setCondition(condition);
+		if(testTask.getCondition() == tmp_condition)
+			assert(true);
+		else
+			fail("was unable to set condition correctly");
+			
+		
+		//test setOwner
+		String owner = "a really unique owner";
+		testTask.setOwner(owner);
+		if(testTask.getOwner() == owner)
+			assert(true);
+		else
+			fail("unable to get/set owner");
+		
+		//UserStory test
+		if(testTask.getUserStory() != null)
+			assert(true);
+		else
+			fail("unable to get user story");
+		
+		
 	}
 	
-	public void testConstruction(){
-		assert(true);
-	}
-
 }
