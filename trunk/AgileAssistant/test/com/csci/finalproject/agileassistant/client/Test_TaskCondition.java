ff4c28cf9ffa18ccd49edf88aea1927b7a09b789
hebadi89@gmail.com <hebadi89@gmail.com@8a08e298-b036-26f9-6304-ede39f763e67>
2011-11-14 05:31:30 +0000

    started working on client side testing - taking over Task.java and UserStory.java related classes

---
+++ trunk/AgileAssistant/test/com/csci/finalproject/agileassistant/client/Test_TaskCondition.java
@@ -0,0 +1,11 @@
+package com.csci.finalproject.agileassistant.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class Test_TaskCondition extends GWTTestCase {
+
+	public String getModuleName() {
+		return "";
+	}
+
+}
