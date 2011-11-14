3b32c7e124c1e2386b543797981e087474ef5992
hebadi89 <hebadi89@gmail.com>
2011-11-14 03:44:24 +0000

    a lot of issues with IDs -tasks IDs, UserStory IDs, PrimKey IDs unable to get much code coverage

---
+++ AgileAssistant/test/com/csci/finalproject/agileassistant/server/ServerTests.java
@@ -0,0 +1,12 @@
+package com.csci.finalproject.agileassistant.server;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({testTask.class,
+		testUserStory.class })
+public class ServerTests {
+
+}
