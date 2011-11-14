169cd3a3f13a940ba9e780df73de16173aea47b3
hebadi89 <hebadi89@gmail.com>
2011-11-14 03:47:57 +0000

    fixed issues with testPersistentProject.java
    also added to test suite

--- AgileAssistant/test/com/csci/finalproject/agileassistant/server/ServerTests.java
+++ AgileAssistant/test/com/csci/finalproject/agileassistant/server/ServerTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({testTask.class,
+@SuiteClasses({testPersistentProject.class, testTask.class,
 		testUserStory.class })
 public class ServerTests {
 
